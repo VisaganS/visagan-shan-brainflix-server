8be407527d032834786b346182ea868d8987b1aa
Visagan Shan <visagan.shan@gmail.com>
2023-06-03 15:34:38 -0400

    get requests created

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const cors = require('cors');
 const port = process.env.PORT || 8080;
-const videoList =  require('./data/video-details.json');
 
+// const videoList =  require('./data/video-details.json');
 app.use(express.json());
 app.use(cors());
 
+const pullDB = () => {
+    return JSON.parse(fs.readFileSync('./data/video-details.json'));
+  }
+  
+  const writeDB = (data) => {
+    fs.writeFileSync('./data/video-details.json', JSON.stringify(data));
+  }
+  
+
+
 app.get('/videos', (req, res) => {
-  res.json(videoList);
+  let data = pullDB();
+  res.json(data);
 });
 
 app.get('/videos/:id', (req, res) => {
 const videoId = req.params.id;
-const currentVid = videoList.find(video => video.id === videoId);
+let rawData = pullDB();
+
+const currentVid = rawData.find(video => video.id === videoId);
 
 if (!currentVid) {
     res.status(404).json({ error: "Video not found"});
