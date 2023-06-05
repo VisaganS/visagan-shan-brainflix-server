e8043a5cbffd86ae805b970cefd96928a9022b95
Visagan Shan <visagan.shan@gmail.com>
2023-06-05 18:57:38 -0400

    post fixed, image links changed to localhost

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
-const cors = require('cors');
+const fs = require("fs");
+const cors = require("cors");
+const { uuid } = require("uuidv4");
 const port = process.env.PORT || 8080;
 
 // const videoList =  require('./data/video-details.json');
 app.use(express.json());
 app.use(cors());
+app.use('/images', express.static('./public/images'));
 
 const pullDB = () => {
-    return JSON.parse(fs.readFileSync('./data/video-details.json'));
-  }
-  
-  const writeDB = (data) => {
-    fs.writeFileSync('./data/video-details.json', JSON.stringify(data));
-  }
-  
+  return JSON.parse(fs.readFileSync("./data/video-details.json"));
+};
 
+const writeDB = (data) => {
+  fs.writeFileSync("./data/video-details.json", JSON.stringify(data));
+};
 
-app.get('/videos', (req, res) => {
+app.get("/videos", (req, res) => {
   let data = pullDB();
   res.json(data);
 });
 
-app.get('/videos/:id', (req, res) => {
-const videoId = req.params.id;
-let rawData = pullDB();
+app.get("/videos/:id", (req, res) => {
+  const videoId = req.params.id;
+  let rawData = pullDB();
 
-const currentVid = rawData.find(video => video.id === videoId);
+  const currentVid = rawData.find((video) => video.id === videoId);
 
-if (!currentVid) {
-    res.status(404).json({ error: "Video not found"});
-}else{
+  if (!currentVid) {
+    res.status(404).json({ error: "Video not found" });
+  } else {
     res.json(currentVid);
-}
+  }
 });
 
-app.post('/videos', (req, res) => {
-    let data = req.body;
-    
-    fs.writeFile('./data/video-details.json', JSON.stringify(data));
-  });
+app.post("/videos", (req, res) => {
+  let data = {id:uuid(),...req.body};
+  let list = pullDB();
+  list.push(data);
+  writeDB(list);
+  res.send(list);
+});
 
 app.listen(port, () => console.log(`Listening on ${port}`));
